654545ff11126a49ecfba252fb8d85e775bf9f4a
antoineSimon91 <antoine.simon91@gmail.com>
2019-05-26 15:00:34 +0200

    add timer decorator

---
+++ scripts/helpers.py
@@ -0,0 +1,17 @@
+
+# standrad imports
+import time
+
+
+def timer(method):
+    """Print the duration of the decorated function"""
+    def wrap(*args, **kwargs):
+        start_time = time.time()
+        output = method(*args, **kwargs)
+        end_time = time.time()
+        duration = end_time - start_time
+        duration = round(duration, 1)
+        method_name = method.__name__
+        print(f"(TIMER) method '{method_name}' duration: {duration} sec")
+        return output
+    return wrap

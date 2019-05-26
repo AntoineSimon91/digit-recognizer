e933660ebd69c2dd5778f469a1445de1000c2546
antoineSimon91 <antoine.simon91@gmail.com>
2019-05-26 16:56:29 +0200

    plot confusion matrix

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -1,5 +1,9 @@
 
+# standard imports
+import itertools
+
 # third party imports
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -25,4 +29,37 @@ def plot_loss_and_accuracy(history, display=True):
     if display:
         plt.tight_layout()
         plt.show()
+    return fig
+
+
+def plot_confusion_matrix(matrix, normalize=False):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cmap = plt.cm.Blues
+    title = 'Confusion matrix'
+    classes = range(10)
+
+    fig = plt.imshow(matrix, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
+
+    thresh = matrix.max() / 2.
+    for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
+        plt.text(j, i, matrix[i, j],
+                 horizontalalignment="center",
+                 color="white" if matrix[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
     return fig

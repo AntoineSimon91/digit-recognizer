010917505597922b18774753a01e959328c34ef2
antoineSimon91 <antoine.simon91@gmail.com>
2019-05-26 16:53:15 +0200

    Remove tests\ folder

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-
-# standard imports
-from os.path import abspath, join, dirname, pardir
-import sys
-
-# add local scripts path to PYTHON PATH
-scripts_dirpath = abspath(join(dirname(__file__), pardir, "scripts"))
-sys.path.append(scripts_dirpath)
